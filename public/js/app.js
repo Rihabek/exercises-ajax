b567bab9c0629a945118e58aa7e314ac5d01a821
rihab <rihabelkhadiri@hotmail.com>
2019-10-30 16:11:37 +0100

    victory

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,7 @@ $(function() {
 
   });
 
-
+  var daveData
   $("#form").submit(function () {
     var data = {
        name : $(this).find('[name="name"]').val(),
@@ -46,8 +46,55 @@ $(function() {
        type :  $(this).find('[name="type"]').val(),
 
     };
-    console.log(data);
-  })
+    $.ajax({
+      url : "/user",
+      method : "POST",
+      data : data,
+
+      success : function(html){
+        $("#formulaire").html(html);
+      }
+
+
+    });
+  });
+
+  var hours = parseInt($("#h").text());
+  var minutes = parseInt($("#m").text());
+  var secondes = parseInt($("#s").text());
+
+  secondes = secondes + 1
+
+  if (secondes == 60){
+    minutes = minutes + 1
+    secondes = 0
+  }
+
+  if (minutes == 60){
+    hours = hours +1
+    minutes = 0
+  }
+
+  if (hours >23){
+    hours = 0
+  }
+
+  if (hours < 10){
+    hours= '0'+ hours;
+  }
+  if (minutes < 10){
+    minutes = '0'+ minutes;
+  }
+
+  if (secondes < 10){
+    secondes= '0'+secondes;
+  }
+
+  $("#h2").html(hours);
+  $("#m2").html(minutes);
+  $("#s2").html(secondes);
+
+
 
 
 
